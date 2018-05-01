203e7c9592e655d04b0b1ccd0c253df127ee0208
Pablo Rocha Moreira <prm.gredes@gmail.com>
2018-05-01 03:28:41 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,20 +1,7 @@
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
-
-def build_character_hash(alphabet_string)
-  character_hash = {}
-
-  alphabet_string.each_char.with_index do |char, index|
-    character_hash[char] = index
-  end
-
-  character_hash
-end
 def alphabetize(arr)
-  arr.sort_by do |phrase|
-    phrase.chars.map do |c|
-      esperanto_alphabet_hash[c]
-    end
- end
-
+   # code here	   # code here
+  ascii = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
+  arr.sort_by{|string| string.tr(ESPERANTO_ALPHABET, ascii)}
 end
