9020742130bc58b850b4213c43d67fc5f43edb6b
Pablo Rocha Moreira <prm.gredes@gmail.com>
2018-05-01 03:27:14 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,20 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+
+def build_character_hash(alphabet_string)
+  character_hash = {}
+
+  alphabet_string.each_char.with_index do |char, index|
+    character_hash[char] = index
+  end
+
+  character_hash
+end
 def alphabetize(arr)
-  # code here
+  arr.sort_by do |phrase|
+    phrase.chars.map do |c|
+      esperanto_alphabet_hash[c]
+    end
+ end
+
 end
