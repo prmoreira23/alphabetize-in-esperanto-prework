ab7fb1291531aa187a98a797aa29143b56b00359
Pablo Rocha Moreira <prm.gredes@gmail.com>
2018-05-01 03:32:46 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,5 +3,8 @@ require 'pry'
 def alphabetize(arr)
    # code here	   # code here
   ascii = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
-  arr.sort_by{|string| string.tr(ESPERANTO_ALPHABET, ascii)}
+  arr.sort_by{|string| 
+      binding.pry
+      string.tr(ESPERANTO_ALPHABET, ascii)
+  }
 end
